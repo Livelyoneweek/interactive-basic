79c3a34af0275a78ec028c43055e13dc93875322
Livelyoneweek <wlvvwhs@naver.com>
2025-01-18 23:51:21 +0900

    stage 완성, mousemove, resize 이벤트 추가함

--- 3d_scroll_study/js/wall3d.js
+++ 3d_scroll_study/js/wall3d.js
@@ -1,8 +1,33 @@
 (function () {
+  const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  const barElem = document.querySelector(".progress-bar");
+
+  const mousePos = { x: 0, y: 0 };
+  let maxScrollValue = 0;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
   window.addEventListener("scroll", function () {
-    const zMove = (scrollY / maxScrollValue) * 980 - 490;
+    const scrollPer = scrollY / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
+
+    //progress bar
+    barElem.style.width = `${scrollPer * 100}%`;
   });
+
+  window.addEventListener("mousemove", function (e) {
+    mousePos.x = -1 + (e.clientX / this.window.innerWidth) * 2;
+    mousePos.y = 1 - +(e.clientY / this.window.innerHeight) * 2;
+
+    stageElem.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${
+      mousePos.x * 5
+    }deg)`;
+  });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
