6132fa9688341533453eab2ac2ac37947bbff9d4
Livelyoneweek <wlvvwhs@naver.com>
2025-01-20 02:40:14 +0900

    좌,우 움직임 추가

--- 3d_scroll_study/js/Character.js
+++ 3d_scroll_study/js/Character.js
@@ -38,34 +38,87 @@ class Character {
     //바로 이전 스크롤 위치
     this.lastScrollTop = 0;
 
+    this.xPos = info.xPos;
+    this.speed = info.speed;
+
+    this.direction;
+
+    // 좌,우 키 누르고 있어도 한번만 실행되게
+    this.runningState = false;
+    this.rafId;
+
     // 초기화 메서드 호출
-    this.init(info);
+    this.init();
   }
 
   // init 메서드 정의
-  init(info) {
+  init() {
     const self = this;
 
     window.addEventListener("scroll", function () {
-      this.clearTimeout(self.scrollState);
+      window.clearTimeout(self.scrollState);
       if (!self.scrollState) {
         self.mainElem.classList.add("running");
       }
 
-      self.scrollState = this.setTimeout(function () {
+      self.scrollState = window.setTimeout(function () {
         self.scrollState = false;
         self.mainElem.classList.remove("running");
       }, 500);
 
-      if (self.lastScrollTop > scrollY) {
-        // 스크롤 올림
+      if (self.lastScrollTop > window.scrollY) {
         self.mainElem.setAttribute("data-direction", "backward");
       } else {
-        // 스크롤 내림
         self.mainElem.setAttribute("data-direction", "forward");
       }
 
-      self.lastScrollTop = scrollY;
+      self.lastScrollTop = window.scrollY;
+    });
+
+    window.addEventListener("keydown", function (e) {
+      if (self.runningState) return;
+
+      if (e.key === "ArrowLeft") {
+        self.direction = "left";
+        self.mainElem.setAttribute("data-direction", "left");
+        self.mainElem.classList.add("running");
+        self.run(self);
+        self.runningState = true;
+      } else if (e.key === "ArrowRight") {
+        self.direction = "right";
+        self.mainElem.setAttribute("data-direction", "right");
+        self.mainElem.classList.add("running");
+        self.run(self);
+        self.runningState = true;
+      }
+    });
+
+    window.addEventListener("keyup", function () {
+      self.mainElem.classList.remove("running");
+      cancelAnimationFrame(self.rafId);
+      self.runningState = false;
     });
   }
+
+  run = (self) => {
+    if (self.direction == "left") {
+      self.xPos = self.xPos - self.speed;
+    } else if (self.direction == "right") {
+      self.xPos = self.xPos + self.speed;
+    }
+
+    if (self.xPos < 2) {
+      self.xPos = 2;
+    }
+
+    if (self.xPos > 88) {
+      self.xPos = 88;
+    }
+
+    self.mainElem.style.left = `${self.xPos}%`;
+
+    self.rafId = requestAnimationFrame(function () {
+      self.run(self);
+    });
+  };
 }
